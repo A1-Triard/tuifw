5aa685b1d6e0e7626794ff717e7c1eda2be66abf
Warlock <internalmike@gmail.com>
2020-09-08 00:19:32 +0300

    example

--- widget/examples/view.rs
+++ widget/examples/view.rs
@@ -1,16 +1,10 @@
 use boow::Bow;
 use tuifw_screen::{Attr, Color};
-use tuifw_widget::view::{Text, ViewTree, View};
+use tuifw_widget::view::{Text, ViewTree, View, view_base_type};
 use tuifw_widget::view::panels::{CanvasPanel, CanvasLayout};
 use tuifw_widget::view::decorators::{BorderDecorator, border_decorator_type};
 
-fn main() {
-    let screen = unsafe { tuifw_screen::init() }.unwrap();
-    let tree = &mut ViewTree::new(screen, |_| ((), |tree| tree));
-    CanvasPanel::new(tree, tree.root());
-    let view = View::new(tree, tree.root(), |view| ((), view));
-    CanvasLayout::new(tree, view);
-    BorderDecorator::new(tree, view);
+fn double_border(tree: &mut ViewTree, view: View) {
     view.decorator_set_uncond(tree, border_decorator_type().tl(), Some(Text {
         value: Bow::Borrowed(&"╔"), fg: Color::Green, bg: None, attr: Attr::empty()
     }));
@@ -35,4 +29,17 @@ fn main() {
     view.decorator_set_uncond(tree, border_decorator_type().b(), Some(Text {
         value: Bow::Borrowed(&"═"), fg: Color::Green, bg: None, attr: Attr::empty()
     }));
+}
+
+fn main() {
+    let screen = unsafe { tuifw_screen::init() }.unwrap();
+    let tree = &mut ViewTree::new(screen, |_| ((), |tree| tree));
+    CanvasPanel::new(tree, tree.root());
+    let view = View::new(tree, tree.root(), |view| ((), view));
+    CanvasLayout::new(tree, view);
+    BorderDecorator::new(tree, view);
+    double_border(tree, view);
+    view.base_set_distinct(tree, view_base_type().w(), Some(8));
+    view.base_set_distinct(tree, view_base_type().h(), Some(8));
+    ViewTree::update(tree, true);
 }
