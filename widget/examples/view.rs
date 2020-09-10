c257c1a3783bbc3e6c4c3315af74fd750227bddc
Warlock <internalmike@gmail.com>
2020-09-10 08:06:23 +0300

    l_arrow

--- widget/examples/view.rs
+++ widget/examples/view.rs
@@ -1,20 +1,22 @@
 #![deny(warnings)]
 use std::borrow::Cow;
 use dyn_context::ContextExt;
-use tuifw_screen::{Key, Vector, Thickness, HAlign, VAlign, Point};
+use tuifw_screen::{Key, Vector, Thickness, HAlign, VAlign, Point, Side};
 use tuifw_widget::view::{ViewTree, View, view_align_type, view_base_type};
 use tuifw_widget::view::panels::{CanvasPanel, CanvasLayout, canvas_layout_type};
+use tuifw_widget::view::panels::{DockPanel, DockLayout, dock_layout_type};
 use tuifw_widget::view::decorators::{BorderDecorator, border_decorator_type};
+use tuifw_widget::view::decorators::{LabelDecorator, label_decorator_type};
 
 fn double_border(tree: &mut ViewTree, view: View) {
-    view.decorator_set_uncond(tree, border_decorator_type().tl(), Cow::Borrowed(&"╔"));
-    view.decorator_set_uncond(tree, border_decorator_type().tr(), Cow::Borrowed(&"╗"));
-    view.decorator_set_uncond(tree, border_decorator_type().bl(), Cow::Borrowed(&"╚"));
-    view.decorator_set_uncond(tree, border_decorator_type().br(), Cow::Borrowed(&"╝"));
-    view.decorator_set_uncond(tree, border_decorator_type().l(), Cow::Borrowed(&"║"));
-    view.decorator_set_uncond(tree, border_decorator_type().t(), Cow::Borrowed(&"═"));
-    view.decorator_set_uncond(tree, border_decorator_type().r(), Cow::Borrowed(&"║"));
-    view.decorator_set_uncond(tree, border_decorator_type().b(), Cow::Borrowed(&"═"));
+    view.decorator_set_distinct(tree, border_decorator_type().tl(), Cow::Borrowed("╔"));
+    view.decorator_set_uncond(tree, border_decorator_type().tr(), Cow::Borrowed("╗"));
+    view.decorator_set_uncond(tree, border_decorator_type().bl(), Cow::Borrowed("╚"));
+    view.decorator_set_uncond(tree, border_decorator_type().br(), Cow::Borrowed("╝"));
+    view.decorator_set_uncond(tree, border_decorator_type().l(), Cow::Borrowed("║"));
+    view.decorator_set_uncond(tree, border_decorator_type().t(), Cow::Borrowed("═"));
+    view.decorator_set_uncond(tree, border_decorator_type().r(), Cow::Borrowed("║"));
+    view.decorator_set_uncond(tree, border_decorator_type().b(), Cow::Borrowed("═"));
 }
 
 fn main() {
@@ -23,14 +25,20 @@ fn main() {
     let padding = Thickness::align(size, screen.size(), HAlign::Center, VAlign::Center);
     let tree = &mut ViewTree::new(screen, |_| ((), |tree| tree));
     CanvasPanel::new(tree, tree.root());
-    let view = View::new(tree, tree.root(), |view| ((), view));
-    CanvasLayout::new(tree, view);
-    BorderDecorator::new(tree, view);
-    double_border(tree, view);
-    view.align_set_distinct(tree, view_align_type().w(), Some(size.x));
-    view.align_set_distinct(tree, view_align_type().h(), Some(size.y));
-    view.layout_set_distinct(tree, canvas_layout_type().tl(), Point { x: padding.l, y: padding.t });
-    view.base_on(tree, view_base_type().input(), |view, context, input| {
+    let border = View::new(tree, tree.root(), |view| ((), view));
+    CanvasLayout::new(tree, border);
+    BorderDecorator::new(tree, border);
+    double_border(tree, border);
+    border.align_set_distinct(tree, view_align_type().w(), Some(size.x));
+    border.align_set_distinct(tree, view_align_type().h(), Some(size.y));
+    border.layout_set_distinct(tree, canvas_layout_type().tl(), Point { x: padding.l, y: padding.t });
+    DockPanel::new(tree, border);
+    let l_arrow = View::new(tree, border, |view| ((), view));
+    DockLayout::new(tree, l_arrow);
+    LabelDecorator::new(tree, l_arrow);
+    l_arrow.decorator_set_distinct(tree, label_decorator_type().text(), Cow::Borrowed("←"));
+    l_arrow.layout_set_distinct(tree, dock_layout_type().dock(), Some(Side::Left));
+    border.base_on(tree, view_base_type().input(), |border, context, input| {
         let tree = context.get_mut::<ViewTree>().expect("ViewTree required");
         let d = match input.key() {
             (n, Key::Left) | (n, Key::Char('h')) =>
@@ -44,9 +52,9 @@ fn main() {
             (_, Key::Escape) => return tree.quit(),
             _ => return,
         };
-        let tl = view.layout_get(tree, canvas_layout_type().tl()).offset(d);
-        view.layout_set_distinct(tree, canvas_layout_type().tl(), tl);
+        let tl = border.layout_get(tree, canvas_layout_type().tl()).offset(d);
+        border.layout_set_distinct(tree, canvas_layout_type().tl(), tl);
     });
-    view.focus(tree);
+    border.focus(tree);
     while ViewTree::update(tree, true).unwrap() { }
 }
