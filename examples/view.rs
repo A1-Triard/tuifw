d5515a8289c3b79a962c33647bd3879d97876e5a
Warlock <internalmike@gmail.com>
2021-07-05 23:29:45 +0300

    Dispatcher (view example)

--- examples/view.rs
+++ examples/view.rs
@@ -1,5 +1,6 @@
 #![deny(warnings)]
-use dyn_context::StateExt;
+use dep_obj::Dispatcher;
+use dyn_context::{State, StateExt, StateRefMut};
 use either::Right;
 use std::borrow::Cow;
 use tuifw::{Key, Vector, Thickness, HAlign, VAlign, Point, Side, Rect};
@@ -9,9 +10,11 @@ use tuifw::view::panels::{ViewBuilderDockPanelExt};
 use tuifw::view::decorators::{ViewBuilderBorderDecoratorExt};
 use tuifw::view::decorators::{ViewBuilderLabelDecoratorExt};
 
-fn build(tree: &mut ViewTree, bounds: Rect) -> View {
+fn build(state: &mut dyn State, bounds: Rect) -> View {
+    let tree: &ViewTree = state.get();
+    let root = tree.root();
     let mut border = None;
-    tree.root().build(tree, |view| view
+    root.build(state, |view| view
         .canvas_panel(|panel| panel
             .child(Some(&mut border), (), |layout| layout.tl(bounds.tl), |view| view
                 .align(|align| align
@@ -67,24 +70,31 @@ fn main() {
     let padding = Thickness::align(Vector { x: 13, y: 7 }, screen.size(), HAlign::Center, VAlign::Center);
     let bounds = padding.shrink_rect(Rect { tl: Point { x: 0, y: 0 }, size: screen.size() });
     let tree = &mut ViewTree::new(screen, |_| ((), |tree| tree));
-    let border = build(tree, bounds);
-    border.base(tree).on(ViewBase::INPUT, |context, border, input| {
-        let tree: &mut ViewTree = context.get_mut();
-        let d = match input.key() {
-            (n, Key::Left) | (n, Key::Char('h')) =>
-                -Vector { x: (n.get() as i16).wrapping_mul(2), y: 0 },
-            (n, Key::Right) | (n, Key::Char('l')) =>
-                Vector { x: (n.get() as i16).wrapping_mul(2), y: 0 },
-            (n, Key::Up) | (n, Key::Char('k')) =>
-                -Vector { x: 0, y: n.get() as i16 },
-            (n, Key::Down) | (n, Key::Char('j')) =>
-                Vector { x: 0, y: n.get() as i16 },
-            (_, Key::Escape) => return tree.quit(),
-            _ => return,
-        };
-        let tl = border.layout_ref(tree).get(CanvasLayout::TL).offset(d);
-        border.layout_mut(tree).set_distinct(CanvasLayout::TL, tl);
-    });
-    border.focus(tree);
-    while ViewTree::update(tree, true).unwrap() { }
+    let dispatcher = &mut Dispatcher::new();
+    tree.merge_mut_and_then(|state| {
+        let border = build(state, bounds);
+        let tree: &mut ViewTree = state.get_mut();
+        border.base(tree).on(ViewBase::INPUT, |state, border, input| {
+            let tree: &mut ViewTree = state.get_mut();
+            let d = match input.key() {
+                (n, Key::Left) | (n, Key::Char('h')) =>
+                    -Vector { x: (n.get() as i16).wrapping_mul(2), y: 0 },
+                (n, Key::Right) | (n, Key::Char('l')) =>
+                    Vector { x: (n.get() as i16).wrapping_mul(2), y: 0 },
+                (n, Key::Up) | (n, Key::Char('k')) =>
+                    -Vector { x: 0, y: n.get() as i16 },
+                (n, Key::Down) | (n, Key::Char('j')) =>
+                    Vector { x: 0, y: n.get() as i16 },
+                (_, Key::Escape) => return tree.quit(),
+                _ => return,
+            };
+            let tl = border.layout_ref(tree).get(CanvasLayout::TL).offset(d);
+            border.layout_mut(state).set_distinct(CanvasLayout::TL, tl);
+        });
+        border.focus(tree);
+        while ViewTree::update(state, true).unwrap() {
+            Dispatcher::dispatch(state);
+        }
+        while Dispatcher::dispatch(state) { }
+    }, dispatcher);
 }
