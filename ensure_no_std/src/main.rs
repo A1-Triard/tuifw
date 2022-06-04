217e5ba91fa8aba07d1d02afde32c81b7f39d814
Warlock <internalmike@gmail.com>
2022-06-05 02:33:16 +0300

    ens

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -7,12 +7,10 @@
 
 use core::alloc::Layout;
 use core::panic::PanicInfo;
-use dep_obj::binding::Bindings;
-use dyn_context::{State, Stop};
 #[cfg(not(windows))]
 use libc::exit;
 use libc_alloc::LibcAlloc;
-use tuifw::WidgetTree;
+use tuifw_window::{RenderPort, WindowTree, Window};
 #[cfg(windows)]
 use winapi::shared::minwindef::UINT;
 #[cfg(windows)]
@@ -41,20 +39,11 @@ pub fn rust_oom(_layout: Layout) -> ! {
     unsafe { exit(98) }
 }
 
-#[derive(State, Stop)]
-struct App {
-    #[state(part)]
-    bindings: Bindings,
-    #[state]
-    widgets: WidgetTree,
-}
+fn render<State: ?Sized>(_: &WindowTree<State>, _: Option<Window>, _: &mut RenderPort, _: &mut State) { }
 
 #[start]
 pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
     let screen = unsafe { tuifw_screen::init() }.unwrap();
-    let mut bindings = Bindings::new();
-    let widgets = WidgetTree::new(screen, &mut bindings);
-    let app = &mut App { bindings, widgets };
-    App::stop(app);
+    let _windows = WindowTree::<()>::new(screen, render);
     0
 }
