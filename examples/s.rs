59b3eb324efb1beade13ea5e93bc4f7cadb777b2
Warlock <internalmike@gmail.com>
2023-09-19 06:22:09 +0300

    examples

---
+++ examples/s.rs
@@ -0,0 +1,38 @@
+#![windows_subsystem = "windows"]
+
+#![deny(warnings)]
+
+use tuifw_screen::{Bg, Fg, Point, Rect, Vector};
+use tuifw_window::{RenderPort, Window, WindowTree};
+use tuifw::LineEdit;
+
+fn render(
+    _tree: &WindowTree<Model>,
+    window: Option<Window>,
+    rp: &mut RenderPort,
+    state: &mut Model
+) {
+    if let Some(window) = window {
+        state.a.render(window, rp);
+    } else {
+        rp.fill(|rp, p| rp.out(p, Fg::Black, Bg::None, " "));
+    }
+}
+
+struct Model {
+    a: LineEdit,
+}
+
+fn main() {
+    let screen = unsafe { tuifw_screen::init(None, None) }.unwrap();
+    let tree = &mut WindowTree::new(screen, render).unwrap();
+    let mut a = LineEdit::new(tree, None, None).unwrap();
+    a.move_xy(tree, Rect { tl: Point { x: 0, y: 0 }, size: Vector { x: 20, y: 1 } });
+    a.line_mut(tree, |line| line.push_str("0.0"));
+    let mut model = Model { a };
+    loop {
+        if let Some(e) = tree.update(true, &mut model).unwrap() {
+            model.a.update(tree, e);
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,17 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
-use alloc::vec::Vec;
-use components_arena::{Arena, Id, Component};
-use core::fmt::Debug;
-use macro_attr_2018::macro_attr;
-use phantom_type::PhantomType;
-use tuifw_screen_base::{Bg, Event, Fg, Point, Range1d, Rect, Vector};
+//use alloc::boxed::Box;
+use alloc::string::String;
+//use alloc::vec::Vec;
+//use components_arena::{Arena, Id, Component};
+//use core::fmt::Debug;
+use core::ops::Range;
+//use macro_attr_2018::macro_attr;
+//use phantom_type::PhantomType;
+use tuifw_screen_base::{Bg, Event, Fg, Key, Point, Range1d, Rect};
 use tuifw_window::{RenderPort, Window, WindowTree};
+use unicode_width::UnicodeWidthChar;
 
 pub trait RenderPortExt {
     fn fill_bg(&mut self, bg: Bg);
@@ -68,133 +71,109 @@ impl RenderPortExt for RenderPort {
     }
 }
 
-pub trait WindowOwner<State: ?Sized> {
-    type Error;
-
-    fn error_priority(&self) -> u8;
-
-    fn move_xy(&mut self, tree: &mut WindowTree<State>, bounds: Rect) -> Result<(), Self::Error>;
+pub struct LineEdit {
+    fg: Fg,
+    bg: Bg,
+    line: String,
+    is_active: bool,
+    window: Window,
+    view: Range<usize>,
+    cursor: usize,
 }
 
-struct WindowAsWindowOwner<Error: Debug>(Window, PhantomType<Error>);
-
-impl<State: ?Sized, Error: Debug> WindowOwner<State> for WindowAsWindowOwner<Error> {
-    type Error = Error;
-
-    fn error_priority(&self) -> u8 { 0 }
-
-    fn move_xy(&mut self, tree: &mut WindowTree<State>, bounds: Rect) -> Result<(), Error> {
-        self.0.move_xy(tree, bounds);
-        Ok(())
-    }
-}
-
-pub struct WindowManager<State: ?Sized, Error: Debug> {
-    windows: Vec<(Box<dyn WindowOwner<State, Error=Error>>, fn(Vector) -> Rect)>
-}
-
-impl<State: ?Sized, Error: Debug> WindowManager<State, Error> {
-    pub fn new() -> Self {
-        WindowManager { windows: Vec::new() }
-    }
-
-    pub fn update(&mut self, tree: &mut WindowTree<State>, event: Event) -> Result<(), Error> {
-        if event == Event::Resize {
-            let screen_size = tree.screen_size();
-            for (window_owner, bounds) in &mut self.windows {
-                window_owner.move_xy(tree, bounds(screen_size))?;
-            }
-        }
-        Ok(())
-    }
-
-    pub fn add_window_owner(
-        &mut self,
-        tree: &mut WindowTree<State>,
-        mut window_owner: Box<dyn WindowOwner<State, Error=Error>>,
-        bounds: fn(Vector) -> Rect
-    ) -> Result<(), Error> {
-        self.windows.try_reserve(1).expect("OOM");
-        let initial_bounds = bounds(tree.screen_size());
-        window_owner.move_xy(tree, initial_bounds)?;
-        let index = self.windows.binary_search_by_key(
-            &(u8::MAX - window_owner.error_priority()),
-            |x| u8::MAX - x.0.error_priority()
-        ).unwrap_or_else(|x| x);
-        self.windows.insert(index, (window_owner, bounds));
-        Ok(())
-    }
-
-    pub fn add_window(
-        &mut self,
-        tree: &mut WindowTree<State>,
-        window: Window,
-        bounds: fn(Vector) -> Rect
-    ) where Error: 'static {
-        self.add_window_owner(tree, Box::new(WindowAsWindowOwner(window, PhantomType::new())), bounds).unwrap();
-    }
-
-    pub fn new_window(
-        &mut self,
+impl LineEdit {
+    pub fn new<State: ?Sized>(
         tree: &mut WindowTree<State>,
         parent: Option<Window>,
-        prev: Option<Window>,
-        bounds: fn(Vector) -> Rect
-    ) -> Result<Window, tuifw_screen_base::Error> where Error: 'static {
+        prev: Option<Window>
+    ) -> Result<Self, tuifw_screen_base::Error> {
         let window = Window::new(tree, parent, prev)?;
-        self.add_window(tree, window, bounds);
-        Ok(window)
-    }
-}
-
-impl<State: ?Sized, Error: Debug> Default for WindowManager<State, Error> {
-    fn default() -> Self { Self::new() }
-}
-
-macro_attr! {
-    #[derive(Component!(class=WindowRenderFnClass))]
-    struct WindowRenderFn<State: ?Sized>(fn(&WindowTree<State>, Window, &mut RenderPort, &mut State));
-}
-
-pub struct WindowRenderer<State: WindowRendererState + ?Sized + 'static> {
-    render_fns: Arena<WindowRenderFn<State>>,
-}
-
-pub trait WindowRendererState {
-    fn window_renderer(&self) -> &WindowRenderer<Self>;
-}
-
-impl<State: WindowRendererState + ?Sized + 'static> WindowRenderer<State> {
-    pub fn new() -> Self {
-        WindowRenderer { render_fns: Arena::new() }
+        Ok(LineEdit {
+            fg: Fg::White,
+            bg: Bg::Blue,
+            line: String::new(),
+            is_active: false,
+            window,
+            cursor: 0,
+            view: 0 .. 0
+        })
     }
 
-    pub fn add_window(
-        &mut self,
-        window: Window,
-        tree: &mut WindowTree<State>,
-        render: fn(&WindowTree<State>, Window, &mut RenderPort, &mut State)
-    ) {
-        let tag = self.render_fns.insert(|tag| (WindowRenderFn(render), tag));
-        window.set_tag(tree, tag);
+    pub fn fg(&self) -> Fg { self.fg }
+
+    pub fn set_fg<State: ?Sized>(&mut self, tree: &mut WindowTree<State>, fg: Fg) {
+        self.fg = fg;
+        self.window.invalidate(tree);
+    }
+
+    pub fn bg(&self) -> Bg { self.bg }
+
+    pub fn set_bg<State: ?Sized>(&mut self, tree: &mut WindowTree<State>, bg: Bg) {
+        self.bg = bg;
+        self.window.invalidate(tree);
+    }
+
+    pub fn window(&self) -> Window { self.window }
+
+    pub fn move_xy<State: ?Sized>(&mut self, tree: &mut WindowTree<State>, bounds: Rect) {
+        self.window.move_xy(tree, bounds);
+        self.update_view(tree);
+    }
+
+    fn update_view<State: ?Sized>(&mut self, tree: &WindowTree<State>) {
+        let bounds = self.window.bounds(tree);
+        let s = &self.line[self.view.start ..];
+        self.view.end = self.view.start;
+        let mut width = 0;
+        for c in s.chars().filter(|&x| x != '\0' && x.width().is_some()) {
+            let c_width = c.width().unwrap();
+            if usize::from(bounds.w() as u16) - width < c_width { break; }
+            width += c_width;
+            self.view.end += c.len_utf8();
+        }
+        if !self.view.is_empty() && !self.view.contains(&self.cursor) {
+            self.cursor = self.view.end - 1;
+        }
+    }
+
+    pub fn line(&self) -> &String { &self.line }
+
+    pub fn line_mut<State: ?Sized, T>(&mut self, tree: &mut WindowTree<State>, f: impl FnOnce(&mut String) -> T) -> T {
+        let res = f(&mut self.line);
+        self.window.invalidate(tree);
+        self.update_view(tree);
+        res
+    }
+
+    pub fn is_active(&self) -> bool { self.is_active }
+
+    pub fn set_is_active(&mut self, is_active: bool) {
+        self.is_active = is_active;
     }
 
     pub fn render(
-        tree: &WindowTree<State>,
-        window: Option<Window>,
-        rp: &mut RenderPort,
-        state: &mut State,
+        &self,
+        window: Window,
+        port: &mut RenderPort,
     ) {
-        if let Some(window) = window {
-            let tag: Id<WindowRenderFn<State>> = window.tag(tree).unwrap();
-            let render_fn = state.window_renderer().render_fns[tag].0;
-            render_fn(tree, window, rp, state);
-        } else {
-            rp.fill(|rp, p| rp.out(p, Fg::LightGray, Bg::None, " "));
+        if self.window != window { return; }
+        port.out(Point { x: 0, y: 0 }, self.fg, self.bg, &self.line[self.view.clone()]);
+    }
+
+    pub fn update<State: ?Sized>(
+        &mut self,
+        tree: &mut WindowTree<State>,
+        event: Event,
+    ) {
+        if self.is_active { return; }
+        match event {
+            Event::Key(n, Key::Char(c)) => {
+                for _ in 0 .. n.get() {
+                    self.line.insert(self.cursor, c);
+                }
+                self.window.invalidate(tree);
+            },
+            _ => { }
         }
     }
-}
-
-impl<State: WindowRendererState + ?Sized + 'static> Default for WindowRenderer<State> {
-    fn default() -> Self { Self::new() }
 }
