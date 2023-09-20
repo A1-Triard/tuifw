dedbad990a396e16ce1290dda95aa13a9af68edf
Warlock <internalmike@gmail.com>
2023-09-20 22:31:08 +0300

    BoxedView

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,17 @@
 
 extern crate alloc;
 
-//use alloc::boxed::Box;
-use alloc::string::String;
-//use alloc::vec::Vec;
-//use components_arena::{Arena, Id, Component};
+use alloc::boxed::Box;
+//use alloc::string::String;
+use alloc::vec::Vec;
+use components_arena::{Arena, Id, Component};
 //use core::fmt::Debug;
-use core::ops::Range;
-//use macro_attr_2018::macro_attr;
+//use core::ops::Range;
+use macro_attr_2018::macro_attr;
 //use phantom_type::PhantomType;
-use tuifw_screen_base::{Bg, Event, Fg, Key, Point, Range1d, Rect};
-use tuifw_window::{RenderPort, Window, WindowTree};
-use unicode_width::UnicodeWidthChar;
+use tuifw_screen_base::{Bg, /*Event,*/ Fg, /*Key,*/ Point, Range1d /*, Rect*/};
+use tuifw_window::{RenderPort /*, Window, WindowTree*/};
+//use unicode_width::UnicodeWidthChar;
 
 pub trait RenderPortExt {
     fn fill_bg(&mut self, bg: Bg);
@@ -71,111 +71,71 @@ impl RenderPortExt for RenderPort {
     }
 }
 
-pub struct LineEdit {
-    fg: Fg,
-    bg: Bg,
-    line: String,
-    is_active: bool,
-    window: Window,
-    view: Range<usize>,
-    cursor: usize,
+macro_attr! {
+    #[derive(Component!)]
+    pub struct BoxedView(pub Box<dyn TView>);
 }
 
-impl LineEdit {
-    pub fn new<State: ?Sized>(
-        tree: &mut WindowTree<State>,
-        parent: Option<Window>,
-        prev: Option<Window>
-    ) -> Result<Self, tuifw_screen_base::Error> {
-        let window = Window::new(tree, parent, prev)?;
-        Ok(LineEdit {
-            fg: Fg::White,
-            bg: Bg::Blue,
-            line: String::new(),
-            is_active: false,
-            window,
-            cursor: 0,
-            view: 0 .. 0
-        })
+pub struct ViewTag(pub Vec<Id<BoxedView>>);
+
+#[portrait::make]
+pub trait TView {
+    fn view(&self) -> &View;
+
+    fn view_mut(&mut self) -> &mut View;
+
+    fn init(&mut self);
+}
+
+pub struct View {
+}
+
+impl TView for View {
+    fn view(&self) -> &View { self }
+
+    fn view_mut(&mut self) -> &mut View { self }
+
+    fn init(&mut self) {
+    }
+}
+
+impl View {
+    pub fn new() -> Self {
+        View { }
+    }
+}
+
+#[portrait::make]
+pub trait TGroup: TView {
+    fn group(&self) -> &Group;
+
+    fn group_mut(&mut self) -> &mut Group;
+
+    fn add_child(&mut self, view: Box<dyn TView>) -> Id<BoxedView> {
+        self.group_mut().children.insert(move |id| (BoxedView(view), id))
     }
 
-    pub fn fg(&self) -> Fg { self.fg }
-
-    pub fn set_fg<State: ?Sized>(&mut self, tree: &mut WindowTree<State>, fg: Fg) {
-        self.fg = fg;
-        self.window.invalidate(tree);
+    fn remove_child(&mut self, id: Id<BoxedView>) -> Box<dyn TView> {
+        self.group_mut().children.remove(id).0
     }
+}
 
-    pub fn bg(&self) -> Bg { self.bg }
+pub struct Group {
+    pub view_: View,
+    children: Arena<BoxedView>,
+}
 
-    pub fn set_bg<State: ?Sized>(&mut self, tree: &mut WindowTree<State>, bg: Bg) {
-        self.bg = bg;
-        self.window.invalidate(tree);
-    }
+#[portrait::fill(portrait::delegate(View; self.view_))]
+impl TView for Group { }
 
-    pub fn window(&self) -> Window { self.window }
+impl TGroup for Group {
+    fn group(&self) -> &Group { self }
 
-    pub fn move_xy<State: ?Sized>(&mut self, tree: &mut WindowTree<State>, bounds: Rect) {
-        self.window.move_xy(tree, bounds);
-        self.update_view(tree);
-    }
+    fn group_mut(&mut self) -> &mut Group { self }
+}
 
-    fn update_view<State: ?Sized>(&mut self, tree: &WindowTree<State>) {
-        let bounds = self.window.bounds(tree);
-        let s = &self.line[self.view.start ..];
-        self.view.end = self.view.start;
-        let mut width = 0;
-        for c in s.chars().filter(|&x| x != '\0' && x.width().is_some()) {
-            let c_width = c.width().unwrap();
-            if usize::from(bounds.w() as u16) - width < c_width { break; }
-            width += c_width;
-            self.view.end += c.len_utf8();
-        }
-        if !self.view.is_empty() && !self.view.contains(&self.cursor) {
-            self.cursor = self.view.end - 1;
-        }
-    }
-
-    pub fn line(&self) -> &String { &self.line }
-
-    pub fn line_mut<State: ?Sized, T>(&mut self, tree: &mut WindowTree<State>, f: impl FnOnce(&mut String) -> T) -> T {
-        let res = f(&mut self.line);
-        self.window.invalidate(tree);
-        self.update_view(tree);
-        res
-    }
-
-    pub fn is_active(&self) -> bool { self.is_active }
-
-    pub fn set_is_active(&mut self, is_active: bool) {
-        self.is_active = is_active;
-    }
-
-    pub fn render(
-        &self,
-        window: Window,
-        port: &mut RenderPort,
-    ) {
-        if self.window != window { return; }
-        port.fill_bg(self.bg);
-        port.out(Point { x: 0, y: 0 }, self.fg, self.bg, &self.line[self.view.clone()]);
-    }
-
-    pub fn update<State: ?Sized>(
-        &mut self,
-        tree: &mut WindowTree<State>,
-        event: Event,
-    ) {
-        if self.is_active { return; }
-        match event {
-            Event::Key(n, Key::Char(c)) => {
-                for _ in 0 .. n.get() {
-                    self.line.insert(self.cursor, c);
-                }
-                self.update_view(tree);
-                self.window.invalidate(tree);
-            },
-            _ => { }
-        }
+impl Group {
+    pub fn new() -> Self {
+        Group { view_: View::new(), children: Arena::new() }
     }
 }
