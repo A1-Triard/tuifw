9c2a0e7e5ec229f97db912af3992254a04ca7d44
Warlock <internalmike@gmail.com>
2023-09-27 09:05:43 +0300

    update ens

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -23,26 +23,20 @@ mod no_std {
         exit(99)
     }
 
-    #[cfg(windows)]
     #[no_mangle]
-    fn rust_oom(_layout: core::alloc::Layout) -> ! {
-        exit(98)
-    }
+    extern fn rust_eh_personality() { }
 }
 
 use alloc::boxed::Box;
 use core::hint::black_box;
-use tuifw::WindowManager;
+use tuifw::Background;
 use tuifw_screen::Vector;
-use tuifw_window::{RenderPort, WindowTree, Window};
-
-fn render<State: ?Sized>(_: &WindowTree<State>, _: Option<Window>, _: &mut RenderPort, _: &mut State) { }
+use tuifw_window::WindowTree;
 
 #[start]
 pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
     let _ = black_box(tuifw_screen::init);
     let screen = Box::new(tuifw_screen_test::Screen::new(Vector { x: 80, y: 25 }));
-    let _windows = WindowTree::<()>::new(screen, render);
-    let _window_manager = WindowManager::<(), ()>::new();
+    let _tree: WindowTree<()> = Background::new().window_tree(screen).unwrap();
     0
 }
