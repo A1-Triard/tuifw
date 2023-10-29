711d21df4c2c33ea839e2ef6cc8fb53d524b641a
Warlock <internalmike@gmail.com>
2023-10-29 14:40:27 +0300

    some virtualization in items_presenter

--- src/items_presenter.rs
+++ src/items_presenter.rs
@@ -1,9 +1,7 @@
-use crate::{widget, StaticText, StackPanel};
+use crate::{widget, StaticText};
 use alloc::boxed::Box;
-use alloc::collections::VecDeque;
 use alloc::string::ToString;
 use alloc::vec::Vec;
-use core::mem::replace;
 use either::Right;
 use tuifw_screen_base::{Rect, Vector, Error, Fg, Bg};
 use tuifw_window::{Event, RenderPort, Widget, WidgetData, Window, WindowTree, App, Timer, Data};
@@ -12,23 +10,17 @@ use tuifw_window::Visibility;
 pub const CMD_ITEMS_PRESENTER_BIND: u16 = 140;
 pub const CMD_ITEMS_PRESENTER_UNBIND: u16 = 141;
 
-enum Update {
-    Refresh,
-    Clear,
-    Push(Box<dyn Data>),
-    Insert(usize, Box<dyn Data>),
-}
-
 widget! {
     #[widget(ItemsPresenterWidget, init=init)]
     pub struct ItemsPresenter {
-        #[property(copy, on_changed=refresh)]
+        #[property(ref, on_changed=update)]
+        items: Vec<Box<dyn Data>>,
+        #[property(copy, on_changed=on_templates_changed)]
         panel_template: Option<Window>,
-        #[property(copy, on_changed=refresh)]
+        #[property(copy, on_changed=on_templates_changed)]
         item_template: Option<Window>,
         update_timer: Option<Timer>,
-        items: Vec<Window>,
-        update_queue: VecDeque<Update>,
+        templates_changed: bool,
         error: bool,
     }
 }
@@ -40,55 +32,22 @@ impl ItemsPresenter {
         error_text.palette_mut(tree, |palette| {
             palette.set(0, Right((Fg::BrightRed, Bg::Blue)));
         });
-        if let Some(panel_template) = ItemsPresenter::panel_template(tree, window) {
-            panel_template.new_instance(tree, Some(window), None)?;
-        } else {
-            StackPanel::new(tree, Some(window), None)?;
-        }
         Ok(())
-    }
-
-    pub fn clear(tree: &mut WindowTree, window: Window) {
-        let data = window.data_mut::<ItemsPresenter>(tree);
-        data.update_queue.push_back(Update::Clear);
-        Self::update(tree, window);
-    }
-
-    pub fn push(tree: &mut WindowTree, window: Window, item: Box<dyn Data>) {
-        let data = window.data_mut::<ItemsPresenter>(tree);
-        data.update_queue.push_back(Update::Push(item));
-        Self::update(tree, window);
-    }
-
-    pub fn insert(tree: &mut WindowTree, window: Window, index: usize, item: Box<dyn Data>) {
-        let data = window.data_mut::<ItemsPresenter>(tree);
-        data.update_queue.push_back(Update::Insert(index, item));
-        Self::update(tree, window);
-    }
-
-    fn refresh(tree: &mut WindowTree, window: Window) {
-        let data = window.data_mut::<ItemsPresenter>(tree);
-        data.update_queue.push_back(Update::Refresh);
-        Self::update(tree, window);
     }
 
     fn error_text(tree: &WindowTree, window: Window) -> Window {
         let first_child = window.first_child(tree).unwrap();
-        let mut child = first_child;
-        loop {
-            let next = child.next(tree);
-            if next == first_child { break child; }
-            child = next;
-        }
-    }
-
-    fn panel(tree: &WindowTree, window: Window) -> Window {
-        window.first_child(tree).unwrap()
-    }
-
-    fn old_panel(tree: &WindowTree, window: Window) -> Window {
-        let first_child = window.first_child(tree).unwrap();
         first_child.next(tree)
+    }
+
+    fn panel(tree: &WindowTree, window: Window) -> Option<Window> {
+        let first_child = window.first_child(tree).unwrap();
+        let second_child = first_child.next(tree);
+        if first_child == second_child {
+            None
+        } else {
+            Some(first_child)
+        }
     }
 
     fn show_error(tree: &mut WindowTree, window: Window, error: Error) {
@@ -98,17 +57,95 @@ impl ItemsPresenter {
         error_text.set_visibility(tree, Visibility::Visible);
     }
 
+    fn on_templates_changed(tree: &mut WindowTree, window: Window) {
+        window.data_mut::<ItemsPresenter>(tree).templates_changed = true;
+        Self::update(tree, window);
+    }
+        
     fn update(tree: &mut WindowTree, window: Window) {
         let update_timer = Timer::new(tree, 0, Box::new(move |tree, app| {
             let data = window.data_mut::<ItemsPresenter>(tree);
             data.update_timer = None;
-            while let Some(update) = window.data_mut::<ItemsPresenter>(tree).update_queue.pop_front() {
-                let data = window.data_mut::<ItemsPresenter>(tree);
-                if data.error {
-                    data.update_queue.clear();
-                    return;
+            if data.error { return; }
+            if data.templates_changed {
+                data.templates_changed = false;
+                if let Some(panel) = Self::panel(tree, window) {
+                    if let Some(first_item_window) = panel.first_child(tree) {
+                        let mut item_window = first_item_window;
+                        loop {
+                            item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_UNBIND), app);
+                            item_window.set_source(tree, None);
+                            item_window = item_window.next(tree);
+                            if item_window == first_item_window { break; }
+                        }
+                    }
+                    panel.drop_window(tree, app);
                 }
-                Self::do_update(tree, window, update, app);
+                let data = window.data::<ItemsPresenter>(tree);
+                if let Some(item_template) = data.item_template {
+                    if let Some(panel_template) = data.panel_template {
+                        let panel = match panel_template.new_instance(tree, Some(window), None) {
+                            Ok(panel) => panel,
+                            Err(error) => return Self::show_error(tree, window, error),
+                        };
+                        let mut item_index = 0;
+                        let mut prev = None;
+                        while
+                            let Some(item) = window.data::<ItemsPresenter>(tree).items.get(item_index).cloned()
+                        {
+                            let item_window = match item_template.new_instance(tree, Some(panel), prev) {
+                                Ok(item_window) => item_window,
+                                Err(error) => return Self::show_error(tree, window, error),
+                            };
+                            item_window.set_source(tree, Some(item));
+                            item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_BIND), app);
+                            item_index += 1;
+                            prev = Some(item_window);
+                        }
+                    }
+                }
+            } else if let Some(panel) = Self::panel(tree, window) {
+                let mut last_item_window = None;
+                let mut item_index = 0;
+                if let Some(first_item_window) = panel.first_child(tree) {
+                    let mut item_window = first_item_window;
+                    let drop_tail = loop {
+                        let item = window.data::<ItemsPresenter>(tree).items.get(item_index).cloned();
+                        if let Some(item) = item {
+                            item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_UNBIND), app);
+                            item_window.set_source(tree, Some(item));
+                            item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_BIND), app);
+                            item_index += 1;
+                        } else {
+                            break true;
+                        }
+                        last_item_window = Some(item_window);
+                        item_window = item_window.next(tree);
+                        if item_window == first_item_window { break false; }
+                    };
+                    if drop_tail {
+                        loop {
+                            item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_UNBIND), app);
+                            item_window.set_source(tree, None);
+                            let next = item_window.next(tree);
+                            item_window.drop_window(tree, app);
+                            item_window = next;
+                            if item_window == first_item_window { break; }
+                        }
+                    }
+                }
+                let mut prev = last_item_window;
+                let item_template = window.data::<ItemsPresenter>(tree).item_template.unwrap();
+                while let Some(item) = window.data::<ItemsPresenter>(tree).items.get(item_index).cloned() {
+                    let item_window = match item_template.new_instance(tree, Some(panel), prev) {
+                        Ok(item_window) => item_window,
+                        Err(error) => return Self::show_error(tree, window, error),
+                    };
+                    item_window.set_source(tree, Some(item));
+                    item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_BIND), app);
+                    item_index += 1;
+                    prev = Some(item_window);
+                }
             }
         }));
         let data = window.data_mut::<ItemsPresenter>(tree);
@@ -116,93 +153,6 @@ impl ItemsPresenter {
             old_update_timer.drop_timer(tree);
         }
     }
-
-    fn do_update(tree: &mut WindowTree, window: Window, update: Update, app: &mut dyn App) {
-        match update {
-            Update::Refresh => {
-                let items_count = window.data::<ItemsPresenter>(tree).items.len();
-                for i in 0 .. items_count {
-                    let item_window = window.data::<ItemsPresenter>(tree).items[i];
-                    item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_UNBIND), app);
-                }
-                let panel = if let Some(panel_template) = ItemsPresenter::panel_template(tree, window) {
-                    match panel_template.new_instance(tree, Some(window), None) {
-                        Ok(panel) => panel,
-                        Err(error) => return Self::show_error(tree, window, error),
-                    }
-                } else {
-                    match StackPanel::new(tree, Some(window), None) {
-                        Ok(panel) => panel,
-                        Err(error) => return Self::show_error(tree, window, error),
-                    }
-                };
-                for i in (0 .. items_count).rev() {
-                    let item_window = window.data::<ItemsPresenter>(tree).items[i];
-                    let item = item_window.source_mut(tree, |source| source.take());
-                    let item_window = if let Some(item_template) = ItemsPresenter::item_template(tree, window) {
-                        match item_template.new_instance(tree, Some(panel), None) {
-                            Ok(item_window) => item_window,
-                            Err(error) => return Self::show_error(tree, window, error),
-                        }
-                    } else {
-                        match StaticText::new(tree, Some(panel), None) {
-                            Ok(item_window) => item_window,
-                            Err(error) => return Self::show_error(tree, window, error),
-                        }
-                    };
-                    item_window.set_source(tree, item);
-                    item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_BIND), app);
-                    window.data_mut::<ItemsPresenter>(tree).items[i] = item_window;
-                }
-                Self::old_panel(tree, window).drop_window(tree, app);
-            },
-            Update::Clear => {
-                let items = replace(&mut window.data_mut::<ItemsPresenter>(tree).items, Vec::new());
-                for item_window in items {
-                    item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_UNBIND), app);
-                    item_window.set_source(tree, None);
-                    item_window.drop_window(tree, app);
-                }
-            },
-            Update::Push(item) => {
-                let panel = Self::panel(tree, window);
-                let prev = window.data::<ItemsPresenter>(tree).items.last().copied();
-                let item_window = if let Some(item_template) = ItemsPresenter::item_template(tree, window) {
-                    match item_template.new_instance(tree, Some(panel), prev) {
-                        Ok(item_window) => item_window,
-                        Err(error) => return Self::show_error(tree, window, error),
-                    }
-                } else {
-                    match StaticText::new(tree, Some(panel), prev) {
-                        Ok(item_window) => item_window,
-                        Err(error) => return Self::show_error(tree, window, error),
-                    }
-                };
-                item_window.set_source(tree, Some(item));
-                item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_BIND), app);
-                window.data_mut::<ItemsPresenter>(tree).items.push(item_window);
-            },
-            Update::Insert(index, item) => {
-                let panel = Self::panel(tree, window);
-                let prev = index.checked_sub(1).map(|i| window.data::<ItemsPresenter>(tree).items[i]);
-                let item_window = if let Some(item_template) = ItemsPresenter::item_template(tree, window) {
-                    match item_template.new_instance(tree, Some(panel), prev) {
-                        Ok(item_window) => item_window,
-                        Err(error) => return Self::show_error(tree, window, error),
-                    }
-                } else {
-                    match StaticText::new(tree, Some(panel), prev) {
-                        Ok(item_window) => item_window,
-                        Err(error) => return Self::show_error(tree, window, error),
-                    }
-                };
-                item_window.set_source(tree, Some(item));
-                item_window.raise(tree, Event::Cmd(CMD_ITEMS_PRESENTER_BIND), app);
-                window.data_mut::<ItemsPresenter>(tree).items.insert(index, item_window);
-            },
-        }
-    }
-
 }
 
 #[derive(Clone, Default)]
@@ -216,7 +166,7 @@ impl Widget for ItemsPresenterWidget {
             update_timer: None,
             error: false,
             items: Vec::new(),
-            update_queue: VecDeque::new(),
+            templates_changed: false,
         })
     }
 
