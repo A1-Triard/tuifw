3929f1a414b5512fc41716e7a5701cc77319bf77
Warlock <internalmike@gmail.com>
2024-09-20 14:03:18 +0300

    ButtonController

--- src/button.rs
+++ src/button.rs
@@ -2,6 +2,7 @@ use crate::widget;
 use alloc::boxed::Box;
 use alloc::string::String;
 use dynamic_cast::impl_supports_interfaces;
+use phantom_type::PhantomType;
 use tuifw_screen_base::{Key, Point, Rect, Vector, Error};
 use tuifw_window::{Event, RenderPort, Timer, Widget, WidgetData, Window, WindowTree, label_width, label};
 use tuifw_window::{CMD_GOT_PRIMARY_FOCUS, CMD_LOST_PRIMARY_FOCUS, App, Color};
@@ -12,14 +13,126 @@ use tuifw_window::{COLOR_BUTTON_FOCUSED_HOTKEY, COLOR_BUTTON_FOCUSED_DISABLED, C
 pub const CMD_BUTTON_CLICK: u16 = 100;
 
 widget! {
-    #[widget(ButtonWidget, init=init_palette, drop=drop_timers)]
+    #[widget(ButtonWidget, init=init_palette, drop=drop_controller)]
     pub struct Button {
         #[property(str, measure)]
         text: String,
-        click_timer: Option<Timer>,
-        release_timer: Option<Timer>,
         #[property(copy)]
         cmd: u16,
+        controller: ButtonController<Button>,
+    }
+}
+
+pub trait IsButton: WidgetData + Sized {
+    fn controller(&self) -> &ButtonController<Self>;
+    fn controller_mut(&mut self) -> &mut ButtonController<Self>;
+    fn cmd(&self) -> u16;
+    fn label(&self) -> Option<char>;
+}
+
+impl IsButton for Button {
+    fn controller(&self) -> &ButtonController<Self> {
+        &self.controller
+    }
+
+    fn controller_mut(&mut self) -> &mut ButtonController<Self> {
+        &mut self.controller
+    }
+
+    fn cmd(&self) -> u16 {
+        self.cmd
+    }
+
+    fn label(&self) -> Option<char> {
+        label(&self.text)
+    }
+}
+
+pub struct ButtonController<Button: IsButton> {
+    click_timer: Option<Timer>,
+    release_timer: Option<Timer>,
+    _phantom: PhantomType<Button>,
+}
+
+impl<Button: IsButton> ButtonController<Button> {
+    pub fn new() -> Self {
+        ButtonController {
+            click_timer: None,
+            release_timer: None,
+            _phantom: PhantomType::new(),
+        }
+    }
+
+    pub fn drop_controller(&mut self, tree: &mut WindowTree, _app: &mut dyn App) {
+        if let Some(release_timer) = self.release_timer.take() {
+            release_timer.drop_timer(tree);
+        }
+        if let Some(click_timer) = self.click_timer.take() {
+            click_timer.drop_timer(tree);
+        }
+    }
+
+    pub fn is_pressed(&self) -> bool {
+        self.release_timer.is_some()
+    }
+
+    fn click(tree: &mut WindowTree, window: Window) {
+        let click_timer = Timer::new(tree, 0, Box::new(move |tree, app| {
+            let data = window.data_mut::<Button>(tree);
+            data.controller_mut().click_timer = None;
+            if window.actual_is_enabled(tree) {
+                let release_timer = Timer::new(tree, 100, Box::new(move |tree, _app| {
+                    let data = window.data_mut::<Button>(tree);
+                    data.controller_mut().release_timer = None;
+                    window.invalidate_render(tree);
+                }));
+                let data = window.data_mut::<Button>(tree);
+                let cmd = data.cmd();
+                if let Some(old_release_timer) = data.controller_mut().release_timer.replace(release_timer) {
+                    old_release_timer.drop_timer(tree);
+                }
+                window.invalidate_render(tree);
+                window.raise(tree, Event::Cmd(cmd), app);
+            }
+        }));
+        let data = window.data_mut::<Button>(tree);
+        if let Some(old_click_timer) = data.controller_mut().click_timer.replace(click_timer) {
+            old_click_timer.drop_timer(tree);
+        }
+    }
+
+    pub fn update(
+        tree: &mut WindowTree,
+        window: Window,
+        event: Event,
+        _event_source: Window,
+        _app: &mut dyn App,
+    ) -> bool {
+        match event {
+            Event::Key(Key::Enter) => {
+                if window.actual_is_enabled(tree) {
+                    Self::click(tree, window);
+                    true
+                } else {
+                    false
+                }
+            },
+            Event::PostProcessKey(Key::Alt(c)) | Event::PostProcessKey(Key::Char(c)) => {
+                if window.actual_is_enabled(tree) {
+                    let data = window.data_mut::<Button>(tree);
+                    let label = data.label();
+                    if Some(c) == label {
+                        Self::click(tree, window);
+                        true
+                    } else {
+                        false
+                    }
+                } else {
+                    false
+                }
+            },
+            _ => false
+        }
     }
 }
 
@@ -37,38 +150,8 @@ impl Button {
         Ok(())
     }
 
-    fn drop_timers(&mut self, tree: &mut WindowTree, _app: &mut dyn App) {
-        if let Some(release_timer) = self.release_timer.take() {
-            release_timer.drop_timer(tree);
-        }
-        if let Some(click_timer) = self.click_timer.take() {
-            click_timer.drop_timer(tree);
-        }
-    }
-
-    fn click(tree: &mut WindowTree, window: Window) {
-        let click_timer = Timer::new(tree, 0, Box::new(move |tree, app| {
-            let data = window.data_mut::<Button>(tree);
-            data.click_timer = None;
-            if window.actual_is_enabled(tree) {
-                let release_timer = Timer::new(tree, 100, Box::new(move |tree, _app| {
-                    let data = window.data_mut::<Button>(tree);
-                    data.release_timer = None;
-                    window.invalidate_render(tree);
-                }));
-                let data = window.data_mut::<Button>(tree);
-                let cmd = data.cmd;
-                if let Some(old_release_timer) = data.release_timer.replace(release_timer) {
-                    old_release_timer.drop_timer(tree);
-                }
-                window.invalidate_render(tree);
-                window.raise(tree, Event::Cmd(cmd), app);
-            }
-        }));
-        let data = window.data_mut::<Button>(tree);
-        if let Some(old_click_timer) = data.click_timer.replace(click_timer) {
-            old_click_timer.drop_timer(tree);
-        }
+    fn drop_controller(&mut self, tree: &mut WindowTree, app: &mut dyn App) {
+        self.controller.drop_controller(tree, app);
     }
 }
 
@@ -81,9 +164,8 @@ impl Widget for ButtonWidget {
     fn new(&self) -> Box<dyn WidgetData> {
         Box::new(Button {
             text: String::new(),
-            release_timer: None,
-            click_timer: None,
             cmd: CMD_BUTTON_CLICK,
+            controller: ButtonController::new()
         })
     }
 
@@ -108,7 +190,7 @@ impl Widget for ButtonWidget {
         let focused = window.is_focused(tree);
         let is_enabled = window.actual_is_enabled(tree);
         let data = window.data::<Button>(tree);
-        let pressed = data.release_timer.is_some();
+        let pressed = data.controller.is_pressed();
         let (color, color_hotkey) = if pressed {
             (6, 6)
         } else if focused {
@@ -153,39 +235,17 @@ impl Widget for ButtonWidget {
         tree: &mut WindowTree,
         window: Window,
         event: Event,
-        _event_source: Window,
-        _app: &mut dyn App,
+        event_source: Window,
+        app: &mut dyn App,
     ) -> bool {
         match event {
             Event::Cmd(CMD_GOT_PRIMARY_FOCUS) | Event::Cmd(CMD_LOST_PRIMARY_FOCUS) |
             Event::Cmd(CMD_GOT_SECONDARY_FOCUS) | Event::Cmd(CMD_LOST_SECONDARY_FOCUS) => {
                 window.invalidate_render(tree);
-                false
             },
-            Event::Key(Key::Enter) => {
-                if window.actual_is_enabled(tree) {
-                    Button::click(tree, window);
-                    true
-                } else {
-                    false
-                }
-            },
-            Event::PostProcessKey(Key::Alt(c)) | Event::PostProcessKey(Key::Char(c)) => {
-                if window.actual_is_enabled(tree) {
-                    let data = window.data_mut::<Button>(tree);
-                    let label = label(&data.text);
-                    if Some(c) == label {
-                        Button::click(tree, window);
-                        true
-                    } else {
-                        false
-                    }
-                } else {
-                    false
-                }
-            },
-            _ => false
+            _ => { },
         }
+        <ButtonController::<Button>>::update(tree, window, event, event_source, app)
     }
 
     fn post_process(&self) -> bool { true }
